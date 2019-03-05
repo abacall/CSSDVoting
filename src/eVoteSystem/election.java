e4ee189801e13a37b6638f2f3655b989180eaf25
1alex <1alex@DESKTOP-L0PHU27>
2019-03-05 16:53:02 +0000

    Completed skeleton of methods and variables in election class

--- src/eVoteSystem/election.java
+++ src/eVoteSystem/election.java
@@ -1,10 +1,15 @@
 package eVoteSystem;
+import java.util.ArrayList;
 import java.util.Date;
 
 
 public class election {
 
 	private String electionID;
+	private electoralRoll voters;
+	private ArrayList<standing> candidates;
+	private votingSystem voteSystem;
+	private ArrayList<administor> administrators;
 	public Date startTime;
 	public Date endTime;
 	
@@ -12,5 +17,26 @@ public class election {
 	{
 		
 	}
+	
+	public ArrayList<standing> returnCandidates()
+	{}
+	
+	public String returnError()
+	{}
+	
+	public boolean validateUser()
+	{}
+	
+	public boolean castVote()
+	{}
+	
+	public void login()
+	{}
+	
+	public void adminLogin()
+	{}
+	
+	public int getVoteCount()
+	{}
 	
 }
