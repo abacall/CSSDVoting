23c34254f2fa46570b24a69a6f67fcfea4d5dad9
abacall <aranbacall@gmail.com>
2019-03-08 19:16:02 +0000

    Begun creating the UI for the Election Selection Page

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,8 @@
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -6,12 +10,14 @@ import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
@@ -21,57 +27,27 @@ import javafx.scene.paint.Color;
 
 public class HelloWorld extends Application {
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-
-		primaryStage.setTitle("JavaFX Welcome");
-		GridPane grid = new GridPane();
-		grid.setAlignment(Pos.CENTER);
-		grid.setHgap(10);
-		grid.setVgap(10);
-		grid.setPadding(new Insets(25, 25, 25, 25));
-
-		Text scenetitle = new Text("Welcome");
-		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-		grid.add(scenetitle, 0, 0, 2, 1);
-
-		Label userName = new Label("User Name:");
-		grid.add(userName, 0, 1);
-
-		TextField userTextField = new TextField();
-		grid.add(userTextField, 1, 1);
-
-		Label pw = new Label("Password:");
-		grid.add(pw, 0, 2);
-
-		PasswordField pwBox = new PasswordField();
-		grid.add(pwBox, 1, 2);
-		
-		Button btn = new Button("Sign in");
-		HBox hbBtn = new HBox(10);
-		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-		hbBtn.getChildren().add(btn);
-		grid.add(hbBtn, 1, 4);
-
-		final Text actiontarget = new Text();
-		grid.add(actiontarget, 1, 6);
-
-		btn.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent e) {
-				actiontarget.setFill(Color.FIREBRICK);
-				actiontarget.setText("Sign in button pressed");
-			}
-		});
-
-		Scene scene = new Scene(grid, 300, 275);
-		primaryStage.setScene(scene);
-		primaryStage.show();
-
-	}
-
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	@Override
+    public void start(Stage primaryStage) throws Exception {
+
+		
+		FXMLLoader loader = new FXMLLoader(new File("C:\\Users\\PCofAwesomeness2\\Eclipse\\MyJavaFXProject\\select_election.fxml").toURI().toURL());
+		Parent root = loader.load();
+		
+		
+        // Create the Scene
+        Scene scene = new Scene(root);
+        // Set the Scene to the Stage
+        primaryStage.setScene(scene);
+        // Set the Title to the Stage
+        primaryStage.setTitle("CSSD");
+        // Display the Stage
+        primaryStage.show();
+
+    }
+
 }
