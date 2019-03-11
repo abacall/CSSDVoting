e989fb7a8ec88fa04a164ea56f5fef5900fcbdba
Alex Fox <1alexwfox@gmail.com>
2019-03-11 16:56:31 +0000

    Minor additions

--- src/eVoteSystem/ElectionInterface.java
+++ src/eVoteSystem/ElectionInterface.java
@@ -1,5 +1,22 @@
 package eVoteSystem;
 
-public class ElectionInterface {
+import java.util.List;
 
+public class ElectionInterface {
+	List<Administrator> admins;
+	List<Election> elections;
+	
+	public Election getElection(String electionID)
+	{
+		for(Election item : elections)
+		{
+			if(item.getElectionID().equalsIgnoreCase(electionID))
+					return item;
+		}
+		
+		return null;
+	}
+	
+	
+	
 }
