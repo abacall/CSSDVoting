e658fed97467eabff86074c2241b11342218e378
Alex Fox <1alexwfox@gmail.com>
2019-03-11 16:58:37 +0000

    Added comments for getElection

--- src/eVoteSystem/ElectionInterface.java
+++ src/eVoteSystem/ElectionInterface.java
@@ -6,6 +6,11 @@ public class ElectionInterface {
 	List<Administrator> admins;
 	List<Election> elections;
 	
+	/**
+	 * Returns the Election object associated with an ID
+	 *
+	 * @return      the Election or null if no election found
+	 */
 	public Election getElection(String electionID)
 	{
 		for(Election item : elections)
