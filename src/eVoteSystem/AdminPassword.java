72860e936ae35fdbe8a62465319c831cdc7bf9f9
Alex Fox <1alexwfox@gmail.com>
2019-03-20 16:05:47 +0000

    Changes to hash function and structure

--- src/eVoteSystem/AdminPassword.java
+++ src/eVoteSystem/AdminPassword.java
@@ -1,78 +1,147 @@
 package eVoteSystem;
 
-import java.security.MessageDigest;
+import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 public class AdminPassword {
 		HashMap<String, String> store = new HashMap<>();
 		
-		public void storeUser(String username, String passwordIn) throws NoSuchAlgorithmException, NoSuchProviderException
+		/**
+		 * Stores a secure hash of password
+		 * 
+		 * @param username
+		 * @param password
+		 * @throws NoSuchAlgorithmException
+		 * @throws NoSuchProviderException
+		 * @throws InvalidKeySpecException 
+		 */
+		public void storeUser(String username, String password) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException
 		{
-			byte[] salt = getSalt();
-	         
-	        String securePassword = getSecurePassword(passwordIn, salt);
+	        String securePassword = getSecurePassword(password);
 	        
 	        store.put(username, securePassword);
-	        
-	        
 		}
 		
-		public boolean validateUser(String username, String passwordIn) throws NoSuchAlgorithmException, NoSuchProviderException
+		/**
+		 * Validates user against stored username and passwords
+		 * 
+		 * @param username
+		 * @param passwordIn
+		 * @return valid login
+		 * @throws NoSuchAlgorithmException
+		 * @throws NoSuchProviderException
+		 * @throws InvalidKeySpecException 
+		 */
+		public boolean validateUser(String username, String passwordIn) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException
 		{
-			byte[] salt = getSalt();
+			//
+			String storedPassword = store.get(username);
+			
+			if(storedPassword != null)
+	        {
+				//Seperate 
+				String[] parts = storedPassword.split(":");
+				int numOfIterations = Integer.parseInt(parts[0]);
+				byte[] salt = fromHex(parts[1]);
+				byte[] hash = fromHex(parts[2]);
+	        
+	        	PBEKeySpec spec = new PBEKeySpec(passwordIn.toCharArray(), salt, numOfIterations, hash.length * 8);
+	        	SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+	        	byte[] testHash = skf.generateSecret(spec).getEncoded();
 	         
-	        String securePassword = getSecurePassword(passwordIn, salt);
+	        	int diff = hash.length ^ testHash.length;
+	        	for(int i = 0; i < hash.length && i < testHash.length; i++)
+	        	{
+	            	diff |= hash[i] ^ testHash[i];
+	        	}
+	        	return diff == 0;
+	        }
 	        
-	        String storedHash = store.get("username");
-	        
-	        return securePassword == storedHash;
-	        
-	        
+	        return false;
 	        
 		}
 		
-	    private static String getSecurePassword(String passwordToHash, byte[] salt)
+		/**
+		 * Generates a secure hash based on password passed in
+		 * 
+		 * @param passwordToHash
+		 * @param salt
+		 * @return
+		 * @throws NoSuchProviderException 
+		 * @throws NoSuchAlgorithmException 
+		 * @throws InvalidKeySpecException 
+		 */
+	    private static String getSecurePassword(String password) throws NoSuchAlgorithmException, NoSuchProviderException, InvalidKeySpecException
 	    {
-	        String generatedPassword = null;
-	        try {
-	            // Create MessageDigest instance for MD5
-	            MessageDigest md = MessageDigest.getInstance("MD5");
-	            //Add password bytes to digest
-	            md.update(salt);
-	            //Get the hash's bytes
-	            byte[] bytes = md.digest(passwordToHash.getBytes());
-	            //This bytes[] has bytes in decimal format;
-	            //Convert it to hexadecimal format
-	            StringBuilder sb = new StringBuilder();
-	            for(int i=0; i< bytes.length ;i++)
-	            {
-	                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
-	            }
-	            //Get complete hashed password in hex format
-	            generatedPassword = sb.toString();
-	        }
-	        catch (NoSuchAlgorithmException e) {
-	            e.printStackTrace();
-	        }
-	        return generatedPassword;
+	        int numOfIterations = 1000;
+	        byte[] salt = getSalt();
+	         
+	        PBEKeySpec spec = new PBEKeySpec(password.toCharArray(), salt, numOfIterations);
+	        SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+	        byte[] hash = skf.generateSecret(spec).getEncoded();
+	        return numOfIterations + ":" + toHex(salt) + ":" + toHex(hash);
 	    }
-		
-	    /*
+	    
+	    /**
 	     * Generates salt bytes in order to avoid collisions in hashed passwords.
 	     * 
-	     * */
+	     * @return		Byte array
+	     * @throws NoSuchAlgorithmException
+	     * @throws NoSuchProviderException
+	     */
 	    private static byte[] getSalt() throws NoSuchAlgorithmException, NoSuchProviderException
 	    {
-	        //Always use a SecureRandom generator
-	        SecureRandom sr = SecureRandom.getInstance("SHA1PRNG", "SUN");
-	        //Create array for salt
+	        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+	        
 	        byte[] salt = new byte[16];
-	        //Get a random salt
-	        sr.nextBytes(salt);
-	        //return salt
+	        
+	        random.nextBytes(salt);
+	        
 	        return salt;
 	    }
+	    
+	    /**
+	     * Convert a byte array into a string
+	     * 
+	     * @param array		byte array to convert
+	     * @return			string from array
+	     * @throws NoSuchAlgorithmException
+	     */
+	    private static String toHex(byte[] array) 
+	    {
+	        BigInteger bi = new BigInteger(1, array);
+	        String hex = bi.toString(16);
+	        int paddingLength = (array.length * 2) - hex.length();
+	        if(paddingLength > 0)
+	        {
+	            return String.format("%0"  +paddingLength + "d", 0) + hex;
+	        }else{
+	            return hex;
+	        }
+	    }
+	    
+	    /**
+	     * Convert a string into a byte array
+	     * 
+	     * @param hex	string to convert
+	     * @return		byte array of string
+	     * @throws NoSuchAlgorithmException
+	     */
+	    private static byte[] fromHex(String hex) 
+	    {
+	        byte[] bytes = new byte[hex.length() / 2];
+	        for(int i = 0; i<bytes.length ;i++)
+	        {
+	            bytes[i] = (byte)Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
+	        }
+	        return bytes;
+	    }
+	    
 }
